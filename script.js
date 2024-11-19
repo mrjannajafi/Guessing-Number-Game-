01fad6d0eb563ca3d8dbfc413bb9e72043d53fb9
mrjannajafi <njfymrjan@gmail.com>
2024-11-19 07:32:59 -0800

    Clean Code and Refactoring of Code

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // DOM
 const body = document.querySelector('body');
 const check = document.querySelector('.check');
-const message = document.querySelector('.message');
 const secretNumber = document.querySelector('.number');
 const again = document.querySelector('.again');
 const highScore = document.querySelector('.highscore');
@@ -10,16 +9,20 @@ let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let calcHighScore = 0;
 const scoreNumber = document.querySelector('.score');
+//Refactoring and avoid to repeat code⬇⬇
+const displsyMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 check.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   //Not A Number
   if (!guess) {
-    message.textContent = 'rung number';
+    displsyMessage('rung number');
   }
   // Correct number
   else if (guess === number) {
-    message.textContent = 'You are winner';
+    displsyMessage('You are winner');
     body.style.backgroundColor = '#60b347';
     secretNumber.style.width = '30rem';
     secretNumber.textContent = number;
@@ -28,42 +31,31 @@ check.addEventListener('click', function () {
       highScore.textContent = calcHighScore;
     }
   }
-  //Guess number is Hiegh
-  else if (guess > number) {
+  //Guess number is not correct (learn Refactoring {clean code})
+  else if (guess !== number) {
     if (score > 1) {
-      message.textContent = 'Your guess is hiegh';
+      displsyMessage(
+        guess > number ? 'Your guess is hiegh' : 'Your guess is low'
+      );
       score--;
       scoreNumber.textContent = score;
     } else {
-      message.textContent = 'You Lose Game';
+      displsyMessage('You Lose Game');
       scoreNumber.textContent = 0;
       body.style.backgroundColor = '#f03e3e';
       secretNumber.style.width = '30rem';
       secretNumber.textContent = number;
     }
   }
-  //Guess number is Low
-  else if (guess < number) {
-    if (score > 1) {
-      message.textContent = 'Your guess is low';
-      score--;
-      scoreNumber.textContent = score;
-    } else {
-      message.textContent = 'You Lose Game';
-      scoreNumber.textContent = 0;
-      body.style.backgroundColor = '#f03e3e';
-      secretNumber.style.width = '30rem';
-      secretNumber.textContent = number;
-    }
-  }
+
 });
 document.querySelector('.again').addEventListener('click', function () {
-  score = 20
+  score = 20;
   scoreNumber.textContent = score;
   number = Math.trunc(Math.random() * 20) + 1;
   body.style.backgroundColor = '#222';
   secretNumber.style.width = '15rem';
   secretNumber.textContent = '?';
-  message.textContent = 'Start guessing...';
+  displsyMessage('Start guessing...');
   document.querySelector('.guess').value = '';
 });
